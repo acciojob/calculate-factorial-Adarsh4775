d7280e9bfca25a8c904d6e42d29f4a69e883a736
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-02 21:42:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function calcFactor(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    let factorial = 1;
+    for (let i = 1; i <= num; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
+}
+
+let num = parseFloat(prompt("Enter the number"));
+if (!isNaN(num) && num >= 0) {
+  let result = calcFactor(num);
+  alert("The factorial of " + num + " is " + result);
+} else {
+  alert("Invalid input. Please enter a non-negative number.");
+}
